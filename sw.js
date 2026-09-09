f1ca8bc5849fd1d08d373d8c62720a93f00b2233
openuse9-eng <openuse9@gmail.com>
2026-09-09 18:42:39 +0300

    Update cache for service programme

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='melgc-songbook-v11';
+const CACHE='melgc-songbook-v10';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./hymns-en.json','./hymns-lg.json','./assets/church-logo.png','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
